628f84635e807bb5253e05da70b1d439a15c6cba
Mike <ulianinmiklekh@gmail.com>
2016-03-14 23:34:38 +0200

    make keys for RSA

--- rsa/rsa_logic.py
+++ rsa/rsa_logic.py
@@ -1,3 +1,5 @@
+import random
+import math
 
 
 def is_prime(number):
@@ -8,7 +10,7 @@ def is_prime(number):
     """
     i = 2
     j = 0
-    while i ** 2 <= number and j != 1:
+    while i <= math.sqrt(number) and j != 1:
         if number % i == 0:
             j = 1
         i += 1
@@ -25,6 +27,23 @@ def primes(n, m):
     :param m:
     :return:
     """
+    prime_list = []
+    for number in xrange(n, m+1):
+        if is_prime(number):
+            prime_list.append(number)
+    return prime_list
+
+
+def are_relatively_prime(a, b):
+    """Return ``True`` if ``a`` and ``b`` are two relatively prime numbers.
+
+    Two numbers are relatively prime if they share no common factors,
+    i.e. there is no integer (except 1) that divides both.
+    """
+    for n in range(2, min(a, b) + 1):
+        if a % n == b % n == 0:
+            return False
+    return True
 
 
 def random_prime(bits):
@@ -33,6 +52,42 @@ def random_prime(bits):
     :param bits:
     :return:
     """
+    n_min = 1 << (bits - 1)
+    n_max = (1 << bits) - 1
+
+    start = 1 << (bits // 2 - 1)
+    stop = 1 << (bits // 2 + 1)
+
+    # primes_list = get_primes(start, stop)
+    primes_list = primes(start, stop)
+    while primes_list:
+        p = random.choice(primes_list)
+        primes_list.remove(p)
+        q_candidates = [q for q in primes_list
+                        if n_min <= p * q <= n_max]
+        if q_candidates:
+            q = random.choice(q_candidates)
+            break
+    else:
+        raise AssertionError("cannot find 'p' and 'q' for a key of "
+                             "length={!r}".format(bits))
+    stop = (p - 1) * (q - 1)
+    for e in xrange(3, stop, 2):
+        if are_relatively_prime(e, stop):
+            break
+    else:
+        raise AssertionError("cannot find 'e' with p={!r} "
+                             "and q={!r}".format(p, q))
+
+    for d in xrange(3, stop, 2):
+        if d * e % stop == 1:
+            break
+    else:
+        raise AssertionError("cannot find 'd' with p={!r}, q={!r} "
+                             "and e={!r}".format(p, q, e))
+
+    # That's all. We can build and return the public and private keys.
+    return [p * q, e, p * q, d]
 
 
 def encrypt(encrypt_string, e, n):
@@ -46,12 +101,22 @@ def encrypt(encrypt_string, e, n):
     """
 
 
-def decrypt(s, d, n):
+def decrypt(decrypt_string, d, n):
     """
     Decrypts the secret key (d, n).
     The result should be the source string to its encryption.
-    :param s:
+    :param decrypt_string:
     :param d:
     :param n:
     :return:
     """
+
+
+import time
+
+
+if __name__ == "__main__":
+    start = time.time()
+    test = random_prime(25)
+    print(test)
+    print("Time: %.03f s" % (time.time() - start))
