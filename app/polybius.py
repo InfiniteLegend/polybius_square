c19b7f3d409777950511c11c3fba9d76914735c5
Shelekhov <stanislav.shelekhov@hotmail.com>
2016-03-31 00:40:19 +0300

    Encoding & decoding are complete

--- app/polybius.py
+++ app/polybius.py
@@ -47,7 +47,7 @@ def get_char_square(password=None):
 
     if password:
         chars += uniqify(password)
-        condition = lambda i: not chr(i) in password
+        condition = lambda i: not unichr(i) in password
 
     chars += [unichr(i) for i in xrange(32, 127) if condition(i)]
     chars += [unichr(i) for i in xrange(1040, 1112) if condition(i)]
@@ -86,7 +86,34 @@ def encode(text, password=None):
     })
 
 
+def decode(text, password=None):
+    """
+    Decoding message using polybius algorithm.
+
+    :param text:
+    :param password:
+    :return:
+    """
+
+    chars = get_char_square(password)
+
+    # Getting coordinates for each character.
+    locations = [[location[0][0], location[1][0]] for location in [np.where(chars == char) for char in text]]
+
+    # Writing them inline iterating columns.
+    new_locations = []
+    for location in locations:
+        new_locations += location
+
+    # Devide them into two columns by center and immediately add character.
+    center = int(len(new_locations) / 2)
+    text = u"".join([chars[new_locations[i], new_locations[i + center]] for i in xrange(0, center)])
+
+    return json.dumps({
+        "data": text
+    })
 
 if __name__ == "__main__":
     text = raw_input().decode(sys.stdin.encoding or locale.getpreferredencoding(True))
-    encode(text)
+
+    decode(text)
